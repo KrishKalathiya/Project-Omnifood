f725da8eb4a0f7fee121e11e7d73286451c7a3bb
KrishK0 <82792090+KrishK0@users.noreply.github.com>
2021-04-29 09:51:35 +0530

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -8,12 +8,12 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=-1#form");
+        header("Location: http://krishk0.github.io/Project-Omnifood/index.php?success=-1#form");
         exit;
     }
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "<YOUR EMAIL HERE>";
+    $recipient = "<krishkalathiya@gmail.com>";
 
     // Set the email subject.
     $subject = "New contact from $name";
@@ -30,7 +30,7 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=1#form");
+    header("Location: http:/krishk0.github.io/Project-Omnifood/index.php?success=1#form");
 
 ?>
 
